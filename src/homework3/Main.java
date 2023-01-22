ec2e67fda29ade251b7f25dfbb5a30e343c183c4
yalokudoyar69 <okudoiar@yahoo.com>
2023-01-23 00:32:20 +0200

    homework3

---
+++ src/homework3/Main.java
@@ -0,0 +1,17 @@
+package homework3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 4;
+        int addition = a + b;
+        int subtraction = a - b;
+        int division = a / b;
+        int multiplication = a * b;
+        System.out.println(addition);
+        System.out.println(subtraction);
+        System.out.println(division);
+        System.out.println(multiplication);
+    }
+}
