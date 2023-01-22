7b170ae0e435d27086dabab6ed35eefead257a02
yalokudoyar69 <okudoiar@yahoo.com>
2023-01-23 00:58:56 +0200

    homework3

--- src/homework3/Main.java
+++ src/homework3/Main.java
@@ -1,17 +1,21 @@
 package homework3;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int a = 12;
-        int b = 6;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть 2 числа:");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
         int addition = a + b;
         int subtraction = a - b;
         int division = a / b;
         int multiplication = a * b;
-        System.out.println(addition);
-        System.out.println(subtraction);
-        System.out.println(division);
-        System.out.println(multiplication);
+        System.out.println("Результат додавання\t\t" + a + " + " + b + " = " + addition);
+        System.out.println("Результат віднімання\t" + a + " - " + b + " = " + subtraction);
+        System.out.println("Результат ділення\t\t" + a + " / " + b + " = " + division);
+        System.out.println("Результат ділення\t\t" + a + " * " + b + " = " + multiplication);
     }
 }
