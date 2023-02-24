998e25834780e5c698fbbd288610c31abc9e3ffa
yalokudoyar69 <okudoiar@yahoo.com>
2023-02-24 12:24:14 +0200

    homework12

---
+++ src/homework12/Main.java
@@ -0,0 +1,12 @@
+package homework12;
+
+//Створити клас з усіма блоками для ініціалізації об’єкту,
+//        в яких потрібно вивести на консоль числа, які відображають його порядковий номер ініціалізації.
+
+public class Main {
+
+    public static void main(String[] args) {
+        WorkingWithInitialization initialization = new WorkingWithInitialization();
+        System.out.println(initialization);
+    }
+}
